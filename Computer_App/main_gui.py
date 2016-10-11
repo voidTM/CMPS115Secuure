467113e76747f5e3594487056284953256bfb6c3
legendddhgf <legendddhgf@gmail.com>
2016-10-11 11:26:31 -0700

    Trying the TKinter library out

---
+++ Computer_App/main_gui.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import Tkinter
+import tkMessageBox
+
+top = Tkinter.Tk()
+
+def helloCallBack():
+   tkMessageBox.showinfo( "Hello Python", "Hello World")
+
+B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
+
+B.pack()
+top.mainloop()
