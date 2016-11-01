31fb3837e6da1d19f889eb0e555ccef226f2400c
John King <joscking@ucsc.edu>
2016-11-01 15:53:00 -0700

    added add function, won't add an entry if the exist already exists

--- Computer_App/db.py
+++ Computer_App/db.py
@@ -3,39 +3,41 @@ import mysql.connector
 
 #initalizes database if accounts exists
 def createDB(cursor, table):
-    cursor.execute("""drop table if exists accounts""")
-
-    cursor.execute("""CREATE TABLE %s(
+    cursor.execute("""CREATE TABLE IF NOT EXISTS %s(
             user  text,
             password  text)""" %table)
 
 #inserts into the "accounts" table.
 #Thing to note: table to be inserted into CANNOT be a variable (must be hardcoded)
 def insertToUserTable(cursor, user, pw):
-    cursor.execute("""INSERT INTO accounts values (%s, %s)""", (user, pw))
+    cursor.execute("""INSERT IGNORE INTO accounts values (%s, %s)""", (user, pw))
 
 
+def verMasterLogin(cursor, login, login_pw):
+    query = ("SELECT user, password FROM accounts")
+    cursor.execute(query)
+    for (u, p) in c:
+        if u.lower() == login.lower() and login_pw.lower() == login_pw.lower():
+            return True
+    return False
 
 #Creates connection to local MySQL database
 connection = mysql.connector.connect(user='root', password='passmanager',
                                      host='localhost', database = 'secuure_db')
-####################
-#      Testing     #
-####################
+#####################
+#      Testing      #
+#####################
 
 c = connection.cursor()
 
 createDB(c, "accounts")
 insertToUserTable(c, "John", "test")
+print(verMasterLogin(c, "John", "test"))
+#query = ("SELECT user, password FROM accounts")
+#
+#c.execute(query)
 
-query = ("SELECT user, password FROM accounts")
 
-c.execute(query)
-
-for (u, p) in c:
-    print ("user: ", u, "password: ", p)
-
-connection.commit()
 
 
 connection.close()
