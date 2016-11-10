b57f298ae3a52499c9ab1f2600d2833aee2af41a
sam song <sasong@ucsc.edu>
2016-11-09 21:07:35 -0800

    edited php scripts

---
+++ PhpScripts/login_mysql.php
@@ -0,0 +1,28 @@
+<?php
+    //Setup connection variables
+    $hostname = "localhost";
+    $username = $_POST['arg_usr'];
+    $password = $_POST['arg_pwd'];
+    $dbname = secuure;
+    $result = False;
+
+
+    // Create connection
+    $conn = mysqli_connect($hostname, $username, $password, $dbname);
+   
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+        $result = False;
+    }
+    else {
+        $result = True;
+    }
+
+    mysqli_close($conn);
+    
+    $resultarray = Array("login" => $result);
+    header("Content-Type: application/json");
+    echo json_encode($resultarray);
+    
+?>
---
+++ PhpScripts/read_mysql.php
@@ -0,0 +1,62 @@
+<?php
+    //Setup connection variables
+    $hostname = "localhost";
+    $username = $_POST['arg_usr'];
+    $password = $_POST['arg_pwd'];
+    $dbname = secuure;
+    $usertable= data;
+    $uid = 1;
+    
+    // Fetch user id
+    //Connect to the database
+    $connection = mysql_connect($hostname, $username, $password);
+    mysql_select_db($dbname, $connection);
+    
+    $query = "SELECT id FROM users WHERE username='$username'";
+    
+    //Run the Query
+    $result = mysql_query($query);
+    
+    //Find the id for a particular username
+    if($result)
+    {
+        while($row = mysql_fetch_array($result))
+        {
+            //$records[] = $row;
+            $uid = $row[id];
+        }
+        
+    }
+
+    
+    //Connect to the database
+    $connection = mysql_connect($hostname, $username, $password);
+    mysql_select_db($dbname, $connection);
+ 
+    //Setup our query
+    $query = "SELECT * FROM $usertable WHERE userid=$uid";
+ 
+    //Run the Query
+    $result = mysql_query($query);
+
+    //If the query returned results, loop through each result
+    if($result)
+    {
+        $arr = [];
+        while($row = mysql_fetch_array($result))
+        {
+            $acc = $row[account];
+            $ws = $row[website];
+            $pwd = $row[password];
+            $note = $row[notes];
+            $arr[] =  $row;
+            //header("Content-Type: application/json");
+            //echo $acc . "  |  " . $ws . "  |  " . $pwd . " | " . $note . " ----------- ";
+        }
+        echo json_encode($arr);
+        //header("Content-Type: application/json");
+        //echo json_encode($arr);
+    }
+
+    mysqli_close($connection)
+?>
