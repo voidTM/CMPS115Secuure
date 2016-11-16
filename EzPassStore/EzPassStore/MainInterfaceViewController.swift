76299bd8d981ab04ecfbbeae73b21dc5958c1005
Alex Ou <aou4@ucsc.edu>
2016-11-15 19:02:12 -0800

    table view for account

--- EzPassStore/EzPassStore/MainInterfaceViewController.swift
+++ EzPassStore/EzPassStore/MainInterfaceViewController.swift
@@ -8,29 +8,31 @@
 
 import UIKit
 
-class MainInterfaceViewController: UIViewController{
+class MainInterfaceViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
-    //data passing from viewcontroller to mainintview
-    @IBOutlet weak var loginUserLabel: UILabel!
     var username:String = ""
-    var tableView: UITableView!
+    var accountArray: [String] = []
+    
+
+    @IBOutlet weak var loginUserLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //set loginuserlabel from username passed in from viewcontroller
         loginUserLabel.text = username;
-//        let tableView = UITableView(frame: view.bounds)
-//        view.addSubview(tableView)
-//        self.tableView = tableView
-//        tableView.dataSource = self
-//        tableView.delegate = self
         // Do any additional setup after loading the view.
+        tableView.register(myCell.self, forCellReuseIdentifier: "cellId")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
     }
     
     //segue from signupview to emailverifyviews
@@ -72,31 +74,24 @@ class MainInterfaceViewController: UIViewController{
     @IBAction func signoutButton(_ sender: AnyObject) {
     }
     
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 5;
-//    }
-//    
-//    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-//        
-//        let button : UIButton = UIButton(type:UIButtonType.custom) as UIButton
-//        
-//        button.frame = CGRect(origin: CGPoint(x: 40,y :60), size: CGSize(width: 100, height: 24))
-//        let cellHeight: CGFloat = 44.0
-//        button.center = CGPoint(x: view.bounds.width / 2.0, y: cellHeight / 2.0)
-//        button.backgroundColor = UIColor.red
-//        button.addTarget(self, action:Selector(("buttonClicked:")), for: UIControlEvents.touchUpInside)
-//        button.setTitle("Click Me !", for: UIControlState.normal)
-//        cell.addSubview(button)
-//        return cell
-//    }
 }
 
-//extension MainInterfaceViewController: UITableViewDataSource, UITableViewDelegate {
-//    
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        tableView.deselectRow(at: indexPath, animated: true)
-//    }
-//    
-//}
+class myCell: UITableViewCell
+{
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews()
+    {
+        
+    }
+    
+    
+}
+
