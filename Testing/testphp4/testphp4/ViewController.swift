ba00d14ffe63e7e72a8b5e4dbe1eae958018ef80
skwan <Steven@eduroam-169-233-206-72.ucsc.edu>
2016-11-21 22:36:48 -0800

    Updated testing with password generator

--- Testing/testphp4/testphp4/ViewController.swift
+++ Testing/testphp4/testphp4/ViewController.swift
@@ -12,22 +12,22 @@ class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         var parse_response = [String]()
-        var request = URLRequest(url: URL(string: "http://localhost/~Aou/create_db_mysql_ios.php")!)
+        var request = URLRequest(url: URL(string: "http://98.234.141.183:8080/register_mysql_ios.php")!)
 //        var request = URLRequest(url: URL(string: "http://localhost/~Steven/delete_mysql.php")!)
 //        var request = URLRequest(url: URL(string: "http://localhost/~Steven/edit_mysql.php")!)
-//        var request = URLRequest(url: URL(string: "http://localhost/~Steven/insert_mysql.php")!)
-//        var request = URLRequest(url: URL(string: "http://localhost/~Steven/login_mysql.php")!)
+//        var request = URLRequest(url: URL(string: "http://98.234.141.183:8080/insert_mysql_ios.php")!)
+ //         var request = URLRequest(url: URL(string: "http://localhost/~Steven/login_mysql_ios.php")!)
 //        var request = URLRequest(url: URL(string: "http://localhost/~Steven/read_accounts_mysql.php")!)
-//        var request = URLRequest(url: URL(string: "http://localhost/~Steven/read_mysql.php")!)
+//          var request = URLRequest(url: URL(string: "http://98.234.141.183:8080/read_mysql_ios.php")!)
 //        var request = URLRequest(url: URL(string: "http://localhost/~Steven/register_mysql.php")!)
         
         request.httpMethod = "POST"
-        let postString = "arg_usr=&arg_pwd=" // login_mysql.php / read_accounts_mysql.php
+//        let postString = "arg_usr=&arg_pwd=" // login_mysql.php / read_accounts_mysql.php
 //        let postString = "arg_usr=&arg_pwd=&arg_del_acc=&arg_del_ws=" // delete_mysql.php
 //        let postString = "arg_usr=&arg_pwd=&arg_edit_acc=&arg_edit_ws=&arg_edit_pwd=&arg_edit_note=" // edit_mysql.php
 //        let postString = "arg_usr=&arg_pwd=&arg_add_acc=&arg_add_ws=&arg_add_pwd=&arg_add_note=" // insert_mysql.php
 //        let postString = "arg_usr=&arg_pwd=&arg_read_acc=&arg_read_ws=" // read_mysql.php
-//        let postString = "arg_usr=&arg_pwd=&arg_fname=&arg_lname=" // register_mysql.php
+        let postString = "arg_usr=&arg_pwd=&arg_fname=&arg_lname=" // register_mysql.php
         
         request.httpBody = postString.data(using: .utf8)
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
@@ -50,7 +50,7 @@ class ViewController: UIViewController {
     }
     
     func parseOutput(response: String) -> Array<String>{
-        var strArray = response.components(separatedBy: "|")
+        var strArray = response.components(separatedBy: " ")
         _ = strArray.removeLast()
         return strArray
     }
@@ -65,6 +65,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func passwordGen(_ sender: UIButton) {
+        let genPass = randomString(length:20, flagletter: true, flagnum: false, flagspecial: true)
+        print(genPass)
+    }
+    
+    func randomString(length: Int, flagletter: Bool, flagnum: Bool, flagspecial: Bool) -> String {
+        
+        var letters : NSString = ""
+        
+        if(flagletter && !flagnum && !flagspecial) {
+            letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        }
+        else if(!flagletter && flagnum && !flagspecial) {
+            letters = "abcdefghijklmnopqrstuvwxyz0123456789"
+        }
+        else if(!flagletter && !flagnum && flagspecial) {
+            letters = "abcdefghijklmnopqrstuvwxyz!@#$%^*()"
+        }
+        else if(flagletter && flagnum && !flagspecial) {
+            letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        }
+        else if(flagletter && !flagnum && flagspecial) {
+            letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^*()"
+        }
+        else if(!flagletter && flagnum && flagspecial) {
+            letters = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^*()"
+        }
+        else if(flagletter && flagnum && flagspecial) {
+            letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^*()"
+        }
+        else if(!flagletter && !flagnum && !flagspecial) {
+            letters = "abcdefghijklmnopqrstuvwxyz"
+        }
+        
+        let len = UInt32(letters.length)
+        
+        var randomString = ""
+        
+        for _ in 0 ..< length {
+            let rand = arc4random_uniform(len)
+            var nextChar = letters.character(at: Int(rand))
+            randomString += NSString(characters: &nextChar, length: 1) as String
+        }
+        
+        return randomString
+    }
 
 }
 
